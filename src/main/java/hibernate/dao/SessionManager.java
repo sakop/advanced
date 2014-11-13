8a4eeaae330f2e0d99363ede2c48d865e5ab37ff
qiucheng <sakopqiu@gmail.com>
2014-11-13 23:06:09 +0900

    add aic pages

--- src/main/java/hibernate/dao/SessionManager.java
+++ src/main/java/hibernate/dao/SessionManager.java
@@ -3,16 +3,14 @@ package hibernate.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 public class SessionManager {
     private static SessionFactory factory = null;
     private static void initSessionFactory(){
         Configuration config = new Configuration().configure();
-        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
-                config.getProperties()).buildServiceRegistry();
-        factory = config.buildSessionFactory(serviceRegistry);
+        //        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
+        //                config.getProperties()).buildServiceRegistry();
+        factory = config.buildSessionFactory();
     }
 
     static{
